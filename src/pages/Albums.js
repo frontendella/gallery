9312e2de980e41a27015f173a2e74d2643c2f9a7
Ella R <ryukhinaella@gmail.com>
2022-06-22 22:43:09 -0700

    Move content of album to the component

--- src/pages/Albums.js
+++ src/pages/Albums.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 function Albums() {
+
+  const [data, setData] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+
+    fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
+      .then((res) => res.json())
+      .then((json) => {
+        const data = json.map((elem, index) => {
+          const imageUrl = `https://api.lorem.space/image/album?w=150&h=220&hash=${elem.id}`;
+          const newObject = {
+            id: elem.id,
+            title: elem.title,
+            image: imageUrl,
+          };
+          return newObject;
+        });
+        return data;
+      })
+      .then((json) => {
+        console.log(json);
+        return json;
+      })
+      .then((finalData) => {
+        setError(undefined);
+        setData(finalData);
+      })
+      .catch((e) => {
+        console.error("Errors: ", e);
+        setError(e);
+      });
+  }, []);
   return (
     <div className='albums'>
-        <h1>Albums</h1>
-    </div>
+      {error && <p>Error!: {error.message}</p>}
+
+{data.map((d) => (
+  <div
+    key={d.id}
+    style={{
+      display: 'inline-block',
+      border: '1px solid black',
+      margin: '10px',
+    }}
+  >
+    <img src={d.image} />
+    <p>{d.title}</p>
+  </div>
+))}
+</div>
   )
 }
 
